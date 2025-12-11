af7b929eec0b32cfb379020bdbfdfb3462aa7143
VVMaestro <bmaecmpo@gmail.com>
2025-12-11 21:11:27 +0500

    Fix docker compose

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
-import time
 import logging
-import schedule
-from job import job_func
-import load_env
 import os
+import time
+
+import schedule
+
+import load_env
+from job import job_func
 
 load_env.load()
 
 # Настройка логирования
 logging.basicConfig(
     level=logging.DEBUG if os.getenv("LOG_LEVEL") == "DEBUG" else logging.INFO,
-    format="\n%(name)s → %(levelname)s: %(message)s\n"
+    format="\n%(name)s → %(levelname)s: %(message)s\n",
 )
 
-schedule_logger = logging.getLogger('schedule')
-FileOutputHandler = logging.FileHandler('/app/logs/bot.log')
+schedule_logger = logging.getLogger("schedule")
+FileOutputHandler = logging.FileHandler("app/logs/bot.log")
 schedule_logger.addHandler(FileOutputHandler)
 
-schedule.every(30).seconds.do(lambda : job_func(schedule_logger))
+schedule.every(30).seconds.do(lambda: job_func(schedule_logger))
 
 while True:
     schedule.run_pending()
--- src/run.py
+++ src/run.py
@@ -1,14 +1,14 @@
-from job import job_func
 import logging
+
 import load_env
+from job import job_func
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="\n%(name)s → %(levelname)s: %(message)s\n"
+    level=logging.INFO, format="\n%(name)s → %(levelname)s: %(message)s\n"
 )
 
 load_env.load()
 
-run_logger = logging.getLogger('run')
+run_logger = logging.getLogger("run")
 
 job_func(run_logger)
